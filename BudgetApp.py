b4d7a653d05945f02d55ee586e58c22f004897c5
GONNE <gonne411@gmail.com>
2026-02-02 20:28:32 +0200

    added catergory class with some methods

---
+++ BudgetApp.py
@@ -0,0 +1,29 @@
+class Category:
+    def __init__(self):
+        ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({
+            'amount': amount,
+            'description': description
+        })
+
+    def withdraw(self, amount, description=""):
+        if amount > self.get_balance():
+            return False
+
+        self.ledger.append({
+            'amount': -amount,
+            'description': description
+        })
+        return True
+
+    def get_balance(self, amount):
+        total = 0
+        for item in self.ledger:
+            total += item[amount]
+        return total
+
+
+def create_spend_chart(categories):
+    pass
