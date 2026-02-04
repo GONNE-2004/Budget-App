394a8a405952263f2c063f46dc9211d9c5527ea9
GONNE <gonne411@gmail.com>
2026-02-04 23:42:49 +0200

    added multiple comments

--- BudgetApp.py
+++ BudgetApp.py
@@ -3,12 +3,14 @@ class Category:
         ledger = []
 
     def deposit(self, amount, description=""):
+        """Deposits the specified amount into the category."""
         self.ledger.append({
             'amount': amount,
             'description': description
         })
 
     def withdraw(self, amount, description=""):
+        """Attempts to withdraw the specified amount from the category."""
         if amount > self.get_balance():
             return False
 
@@ -19,6 +21,7 @@ class Category:
         return True
 
     def get_balance(self):
+        """Returns the total balance of the category."""
         total = 0
         for item in self.ledger:
             total += item["amount"]
