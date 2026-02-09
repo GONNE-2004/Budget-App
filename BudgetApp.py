7de7f734083e433f04e7f2acdc1099a86671ecc5
GONNE <gonne411@gmail.com>
2026-02-09 21:44:07 +0200

    added the str method

--- BudgetApp.py
+++ BudgetApp.py
@@ -42,6 +42,21 @@ class Category:
         """Checks if the category has enough funds for the specified amount."""
         return amount <= self.get_balance()  # check if the amount is less than or equal to the current balance
 
+    def __str__(self):
+        output = []
+        # title
+        output.append(self.name.center(30, "*"))
+        # ledger enteries
+        for item in self.ledger:
+            desc = item["description"][:23].ljust(23)
+            amount = f"{item['amount']:.2f}".rjust(7)
+            output.append(f"{desc}{amount}")
+
+        # total
+        output.append(f"Total: {self.get_balance():.2f}")
+
+        return "\n".join(output)
+
 
 def create_spend_chart(categories):
     """Creates a bar chart representing the percentage of total spending for each category."""
